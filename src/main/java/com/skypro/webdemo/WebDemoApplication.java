52ec2ab97cc572e8cb48a71b82b293a416bfafec
leush <leushkinml@mail.ru>
2023-01-19 14:15:51 +0300

    19.01.2023   14:14 Не работает поиск по полям в многофункциональном GET.

--- src/main/java/com/skypro/webdemo/WebDemoApplication.java
+++ src/main/java/com/skypro/webdemo/WebDemoApplication.java
@@ -1,9 +1,11 @@
 package com.skypro.webdemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class WebDemoApplication {
 
     public static void main(String[] args) {
